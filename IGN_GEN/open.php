1c2808084a9d8d59d89ac7ca95fda8d1e81a4add
willyjlee <williamjlee@ucla.edu>
2017-04-17 02:21:09 -0700

    set pins

--- IGN_GEN/open.php
+++ IGN_GEN/open.php
@@ -2,13 +2,13 @@
 	
     include( __DIR__ . "/../read.php");
     
-    $ret = json_decode(getPins("IGN_GEN"), true);
+    $ret = json_decode(getPins("IGN_GEN", 1), true);
     
-    system("gpio -g mode 2 out");
-    system("gpio -g write 2 1");
+    //system("gpio -g mode 2 out");
+    //system("gpio -g write 2 1");
 
     //if status is OK and isset of jsonarray    
  
     $res = array('status' => $ret['status'], 'op' => 'OPEN IGN_GEN');
-    echo json_encode($res);
+    echo json_encode($res)."\n";
 ?>
